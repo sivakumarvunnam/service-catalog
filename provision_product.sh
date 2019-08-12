a3d89c372a8aade340d7897f17fb5b32ff54d2ed
Sivakumar Vunnam <Sivakumar.Vunnam@bhnetwork.com>
2019-08-12 13:28:12 -0500

    updated

--- provision_product.sh
+++ provision_product.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env 
+#!/bin/bash
 platform = $1
 project = $2
 product_name = $3
 environment = $4 
-property_filename=''' + property_filename + '''
 export PROVISIONED_PRODUCT_NAME=${platform}-${project}-${product_name}-${environment}
 echo "PROVISIONED_PRODUCT_NAME is ${PROVISIONED_PRODUCT_NAME}"
